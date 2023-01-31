90740080f687c5a4ee6052b336ae63a0fba13aae
lmhazel <leahazel2000@gmail.com>
2023-01-31 14:23:02 -0800

    completed api validations

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,21 +6,22 @@ class CatsController < ApplicationController
     end
   
     def create
-        # Create a new cat
         cat = Cat.create(cat_params)
-        render json: cat
+        if cat.valid?
+          render json: cat
+        else
+           render json: cat.errors, status: 422
+        end
     end
-    
-      # Handle strong parameters, so we are secure
+      
+    def update
+    end
+      
+    def destroy
+    end
+      
     private
     def cat_params
         params.require(:cat).permit(:name, :age, :enjoys, :image)
     end
-  
-    def update
-    end
-  
-    def destroy
-    end
-  
 end
