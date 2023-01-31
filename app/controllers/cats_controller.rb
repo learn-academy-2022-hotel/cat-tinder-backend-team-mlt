dca0fdb23ebcbedd7dd30ef4cf053a5b567d590e
Torin Jacobs <118698456+TorinJacobs@users.noreply.github.com>
2023-01-31 11:59:12 -0800

    Update cats_controller.rb

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,25 +1,24 @@
 class CatsController < ApplicationController
 
     def index
-      cats = Cat.all
-      render json: cats
+        cats = Cat.all
+        render json: cats
     end
   
     def create
-        # Create a new cat
         cat = Cat.create(cat_params)
         render json: cat
     end
     
-      def update
-      end
+    def update
+    end
       
-      def destroy
-      end
+    def destroy
+    end
       
-      private
-      def cat_params
-          params.require(:cat).permit(:name, :age, :enjoys, :image)
-      end
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :age, :enjoys, :image)
+    end
     
 end
